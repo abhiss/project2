3b10d1f5f972d8287f530abb42bed980d03d07b7
= <abbbbbbbbbbbhi@gmail.com>
2020-01-23 20:56:22 -0800

    save

---
+++ built/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var path = require("path");
+function default_1(app) {
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + "/public/index.html"));
+    });
+    console.log('this ran');
+    app.post('/test', function (req, res) {
+        fs.writeFileSync(__dirname + '/files.ogg', Buffer.from(req.body.test.replace('data:audio/webm;codecs=opus;base64,', ''), 'base64'));
+        res.json('success');
+    });
+}
+exports["default"] = default_1;
