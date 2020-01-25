36ce91c6c29c36754d7161ed1182fd6cdf130002
James Bajamundi <jamesbjmnd@gmail.com>
2020-01-25 13:51:01 -0800

    save

--- built/routes/auth.js
+++
@@ -1,9 +0,0 @@
-var withAuth = function (req, res, next) {
-    if (!req.session.userId) {
-        res.redirect("/login");
-    }
-    else {
-        next();
-    }
-};
-module.exports = withAuth;
