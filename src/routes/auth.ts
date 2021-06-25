3ef8bd9a5ba933c1653b42a84c08e2cb97b86b9c
Abhi Sohal <abbbbbbbbbbbhi@gmail.com>
2021-06-24 20:01:45 -0700

    clean up

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,7 @@
-export { }
-
-const withAuth = (req, res, next) => {
+export const withAuth = (req, res, next) => {
     if (!req.session.userId) {
         res.redirect("/signin");
     } else {
         next();
     }
 };
-
-module.exports = withAuth;
--- src/routes/htmlRoutes.ts
+++ src/routes/htmlRoutes.ts
@@ -1,5 +1,5 @@
-import * as express from 'express'
-import * as fs from 'fs'
+import * as express from 'express';
+import * as fs from 'fs';
 import * as path from 'path';
 import * as request from 'request'
 
